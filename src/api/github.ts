8ab409f6f4f9c79b372dfe32382acb020f9b4eb6
siddheshtodkar <stodkar53@gmail.com>
2025-09-06 14:14:17 +0530

    follow unfollow user

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,4 +1,5 @@
 const API_URL = import.meta.env.VITE_GITHUB_API_URL
+const API_TOKEN = import.meta.env.VITE_GITHUB_API_TOKEN
 
 export const fetchGithubUser = async (username: string) => {
   const res = await fetch(`${API_URL}/users/${username}`)
@@ -14,4 +15,45 @@ export const fetchGithubUserSuggestions = async (username: string) => {
     throw new Error('User Not Found')
   const data = await res.json()
   return data.items
+}
+
+export const checkIfFollowingUser = async (username: string) => {
+  const res = await fetch(`${API_URL}/user/following/${username}`, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+      Accept: 'application/vnd.github+json'
+    }
+  })
+  if (res.status === 204)
+    return true
+  else if (res.status === 404)
+    return false
+  else
+    throw new Error('failed to check follow status')
+}
+
+export const followUser = async (username: string) => {
+  const res = await fetch(`${API_URL}/user/following/${username}`, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+      Accept: 'application/vnd.github+json'
+    }
+  })
+  if (!res.ok)
+    throw new Error('Failed to follow user')
+  return true
+}
+
+export const unFollowUser = async (username: string) => {
+  const res = await fetch(`${API_URL}/user/following/${username}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+      Accept: 'application/vnd.github+json'
+    }
+  })
+  if (!res.ok)
+    throw new Error('Failed to unfollow user')
+  return true
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,13 +1,46 @@
-import { FaGithubAlt } from "react-icons/fa";
+import { FaGithubAlt, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import type { GithubUser } from "../types";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { checkIfFollowingUser, followUser, unFollowUser } from "../api/github";
+import { toast } from "sonner";
 
 const UserCard = ({ user }: { user: GithubUser }) => {
+  const { data: isFollowing, refetch } = useQuery({
+    queryKey: ['follow-status', user.login],
+    queryFn: () => checkIfFollowingUser(user.login),
+    enabled: !!user.login
+  })
+  const { mutate: follow, isPending } = useMutation({
+    mutationFn: () => followUser(user.login),
+    onSuccess: () => {
+      refetch()
+      toast.success(`You are now following ${user.login}`)
+    },
+    onError: (err) => {
+      toast.error(err.message)
+    }
+  })
+  const { mutate: unFollow, isPending: isLoading } = useMutation({
+    mutationFn: () => unFollowUser(user.login),
+    onSuccess: () => {
+      refetch()
+      toast.success(`You are no longer following ${user.login}`)
+    },
+    onError: (err) => {
+      toast.error(err.message)
+    }
+  })
   return (
     <div className="user-card">
       <img src={user.avatar_url} alt={user.name} className="avatar" />
       <h2>{user.name || user.login}</h2>
       <p className="bio">{user.bio}</p>
-      <a href={user.html_url} className="profile-btn" target="_blank" rel="noopener noreferrer"><FaGithubAlt />View GitHub Profile</a>
+      <div className="user-card-buttons">
+        <button className={`follow-btn ${isFollowing ? 'following' : ''}`} onClick={() => { isFollowing ? unFollow() : follow() }} disabled={isPending || isLoading}>
+          {isFollowing ? <><FaUserMinus />Following</> : <><FaUserPlus />Follow User</>}
+        </button>
+        <a href={user.html_url} className="profile-btn" target="_blank" rel="noopener noreferrer"><FaGithubAlt />View GitHub Profile</a>
+      </div>
     </div>
   );
 }
